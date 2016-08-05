7e75989f4f79dccd6af8759f188fd76fcf9b3337
jeichelbaum <trash.meil@gmx.de>
2016-08-05 17:49:39 +0200

    network discovery working

--- Assets/scripts/network/DDNetSimple.cs
+++ Assets/scripts/network/DDNetSimple.cs
@@ -12,21 +12,26 @@ public class DDNetSimple : MonoBehaviour
     static DDNetSimple instance = null;
     DDNetDiscovery netDiscovery;
 
+    string serverAddress = null;
     public int portBroadcast = 7754;
     public int portGame = 7755;
-    float t_searchTimeout = 5f;
+    float t_searchTimeout = 8f;
 
-    bool searching = false;
-    bool server = false;
-    bool client = false;
+    bool isSearching = false;
+    bool isServer = false;
+
+    NetworkClient netClient;
 
     void Awake()
     {
         instance = this;
+
+        // init netdiscovery
         netDiscovery = gameObject.AddComponent<DDNetDiscovery>();
         netDiscovery.ddnet = this;
         netDiscovery.broadcastPort = portBroadcast;
-        //netDiscovery.showGUI = false;
+        netDiscovery.showGUI = false;
+        netDiscovery.useNetworkManager = false;
     }
 
     void Update()
@@ -36,28 +41,31 @@ public class DDNetSimple : MonoBehaviour
 
     void UpdateDiscovery()
     {
-        if (t_searchTimeout < -1f) return;
+        if (netDiscovery == null) return;
         t_searchTimeout -= Time.deltaTime;    // update connection timer
 
         // initially: search for games
-        if (t_searchTimeout > 1f && !searching)
+        if ( !isSearching && t_searchTimeout > 1f)
         {
-            Debug.Log("attempt client");
+            Debug.Log("DDNet: Searching server ...");
             netDiscovery.Initialize();
             netDiscovery.StartAsClient(); // network broadcast client
-            searching = true;
+            isSearching = true;
+        }
+
+        // 1sec to timeout: stop searching
+        if (isSearching && t_searchTimeout < 1f)
+        {
+            isSearching = false;
+            netDiscovery.StopBroadcast();
         }
 
         // after timeout: start server
-        if (!server && t_searchTimeout < 0f)
+        if (!isServer && t_searchTimeout < 0f)
         {
-            // stop searching
-            searching = false;
-            netDiscovery.StopBroadcast();
-
             // start server
-            Debug.Log("start server");
-            server = true;
+            Debug.Log("DDNet: Server started at " + Network.player.ipAddress + ":" + portGame);
+            isServer = true;
             netDiscovery.Initialize();
             netDiscovery.StartAsServer(); // network broadcast server
             NetworkServer.Listen(portGame);
@@ -66,6 +74,24 @@ public class DDNetSimple : MonoBehaviour
 
     public void OnReceivedBroadcast(string fromAddress, string data)
     {
-        Debug.Log("recived broadcast: " + fromAddress);
+        // stop broadcasting
+        Destroy(netDiscovery);
+        netDiscovery = null;
+        isSearching = false;
+
+        // parse address
+        serverAddress = fromAddress.Substring(fromAddress.LastIndexOf(':') + 1);
+        Debug.Log("connecting to: " + serverAddress + ":" + portGame.ToString());
+
+        // connect to server
+        netClient = new NetworkClient();
+        netClient.RegisterHandler(MsgType.Connect, OnConnected);
+        netClient.Connect("192.168.1.8", portGame);
+    }
+
+    // client function
+    public void OnConnected(NetworkMessage netMsg)
+    {
+        Debug.Log("Connected to server");
     }
 }
